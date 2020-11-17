eb0a1da88cdcfa2e588bf4578600f96d5e9810fa
Diego Alexandro de Oliveira <diego.alexandro@totvs.com.br>
2020-11-17 13:28:58 -0300

    primeiro commit

---
+++ src/main/java/br/com/diegoalexandro/desafioagrojava/DesafioAgroJavaApplication.java
@@ -0,0 +1,13 @@
+package br.com.diegoalexandro.desafioagrojava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioAgroJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioAgroJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/diegoalexandro/desafioagrojava/DesafioAgroJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.diegoalexandro.desafioagrojava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioAgroJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
